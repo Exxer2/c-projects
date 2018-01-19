1531389cb00cd8ffc65b60d76e08eee505b7ed88
Exxer2 <reichertmatthewj@gmail.com>
2018-01-19 20:00:10 +0000

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,7 @@ void testRoot();
 /* Main: Begin!! */
 
 int main () {
-  testRoot();
-
+  testRoot(); //Test if we're root.
   char * config_file;
   return 0;
 }
@@ -20,17 +19,23 @@ int main () {
 /* Below this line, we will  flesh out our functions. 
  * Make sure they're declared! */
 /*****************************************************/
-# This tests to see if we are running as root actual and not sudo.
-# We need to actually be root or we cannot grab all the system health
-# data we actually need.
+// This tests to see if we are running as root actual and not sudo.
+// We need to actually be root or we cannot grab all the system health
+// data we actually need.
 void testRoot() {
   uid_t uid=getuid(), euid=geteuid();
-  if (uid<0 || uid!=euid) {
-    printf("Error: \"UID\"<0 or \"UID\"!=\"EUID\". Ending. Check your user prileges.\n(You must be actual root and not running with sudo.)\n");
-    printf("UID=%d, EUID=%d\n",uid,euid);
-    exit(-1);
-    }
-  else {
-    printf("UID = %d, EUID = %d\n",uid,euid);
+  switch (uid, euid) {
+    case (uid==0 & uid==euid) : // Check if we're actually root (0)
+      break; //Return to Main and continue execution
+    case (uid<0 || uid!=euid) : //UID or EUID below zero or not matching could indicate compromise
+      printf("Error: \"UID\"<0 or \"UID\"!=\"EUID\". Ending. Check your user prileges.\n(You must be actual root and not running with sudo.)\n");
+        printf("UID=%d, EUID=%d\n",uid,euid);
+        exit(1); //Exit if that's the case
+    case (uid!=0 || euid!=0) : //Notify the user that they aren't root, but everything else is good
+      printf("You must perform this action as actual root, not with sudo.\n");
+    default:
+      printf("Invalid UID or EUID detected.\n"); //If it fails for unknown reasons...
+      printf("UID = %d, EUID = %d\n",uid,euid);
+      exit(2);
     }
 }
